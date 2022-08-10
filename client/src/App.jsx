c4e494a3d4d6c6f0d028ccdd4c17f8bab0381af8
Dan Damian <arcassis@gmail.com>
2022-08-10 11:09:41 +0300

    Removed some un-necessary componentes from the ui.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,6 @@ function App() {
         <div className="container">
           <Intro />
           <hr />
-          <Setup />
-          <hr />
           <Demo />
           <hr />
           <Footer />
